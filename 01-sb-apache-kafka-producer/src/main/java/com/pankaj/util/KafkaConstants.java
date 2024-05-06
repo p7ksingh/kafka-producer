f0b2d6e41926e44197c2e990072468d165a0529f
Pankaj Singh <p7ksingh@gmail.com>
2024-05-06 23:57:07 +0530

    kafka producer

---
+++ 01-sb-apache-kafka-producer/src/main/java/com/pankaj/util/KafkaConstants.java
@@ -0,0 +1,15 @@
+package com.pankaj.util;
+
+/**
+ * This class is used to declare constants of this application
+ * 
+ * @author Pankaj
+ *
+ */
+public class KafkaConstants {
+
+	public static final String TOPIC = "customer";
+	public static final String GROUP_ID = "group_customer";
+	public static final String HOST = "localhost:9092";
+
+}
